a9ae77a741724dff386754855027f48b397e151f
harrischen <harrischen@tencent.com>
2024-09-05 09:13:24 +0800

    feat: 优化汇立银行的数据获取逻辑

--- crawler/interest-welab.ts
+++ crawler/interest-welab.ts
@@ -14,35 +14,75 @@ import {
  * @param url
  * @returns
  */
-export async function GetWeLabInterestRate(browser: puppeteer.Browser) {
+export async function GetWeLabBankInterestRate(browser: puppeteer.Browser) {
   const output = {
-    bankName: "匯立",
+    bankName: "汇立",
     url: "https://www.welab.bank/zh/feature/gosave_2/",
-    interestRate: {
-      HKD: {} as { [key: string]: string },
+    savings: {
+      HKD: "",
+    },
+    deposit: {
+      minAmt: {
+        HKD: "",
+      },
+      interestRates: {
+        HKD: {} as { [key: string]: string },
+      },
     },
   };
   try {
     const htmlContent = await FetchWebsiteContent(browser, output.url);
-    output.interestRate.HKD = parseHtmlContent(htmlContent);
+    output.savings = getSavingsDetail(htmlContent);
+    output.deposit = getDepositDetail(htmlContent);
     return output;
   } catch (error) {
     return output;
   }
 }
 
-function parseHtmlContent(html: string) {
-  const $ = cheerio.load(html);
-  const output = GetInterestTemplate();
+/**
+ * 获取活期存款
+ * @param html
+ * @returns
+ */
+function getSavingsDetail(html: string) {
+  return {
+    HKD: "",
+    USD: "",
+    CNY: "",
+  };
+}
 
-  $(".data-perks .d-md-block tbody tr").each((_, row) => {
+/**
+ * 获取定期存款
+ * @param html
+ * @returns
+ */
+function getDepositDetail(html: string) {
+  const $ = cheerio.load(html);
+  const hkdOutput = GetInterestTemplate();
+
+  // 找出指定的table
+  const tr = $(".data-perks .d-md-block tbody tr");
+
+  // 通过遍历tr后再遍历td的形式获取相应的存期入利率
+  tr.each((_, row) => {
     const tableDataCell = $(row).find("td");
     const rate = $(tableDataCell).eq(1).text().trim();
     const period = FormatPeriod($(tableDataCell).eq(0).text().trim());
-    if (rate && period && output[period] === "") {
-      output[period] = rate;
+    if (rate && period && hkdOutput[period] === "") {
+      hkdOutput[period] = rate;
     }
   });
 
-  return FormatInterestOutput(output);
+  return {
+    minAmt: {
+      HKD: "-",
+    },
+    interestRates: {
+      HKD: FormatInterestOutput(hkdOutput),
+      CNY: [],
+      USD: [],
+    },
+  };
 }
