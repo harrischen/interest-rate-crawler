f9f35b0b696802340732a7e82661664ad4efd4a6
harrischen <harrischen@tencent.com>
2024-10-14 10:21:58 +0800

    fix: 优化天星银行的数据获取逻辑

--- crawler/interest-Airstar.ts
+++ crawler/interest-Airstar.ts
@@ -83,7 +83,7 @@ function getDetailWithHKD(html: string) {
   output["1M"] = FormatRate(tr.eq(2).find("td").eq(1).text());
   output["3M"] = FormatRate(tr.eq(4).find("td").eq(1).text());
   output["6M"] = FormatRate(tr.eq(6).find("td").eq(1).text());
-  output["12M"] = FormatRate(tr.eq(8).find("td").eq(1).text());
+  output["12M"] = FormatRate(tr.eq(7).find("td").eq(1).text());
 
   return {
     title: "星息定存 提取自如",
@@ -97,11 +97,11 @@ function getDetailWithUSD(html: string) {
   const output = GetInterestTemplate();
 
   const tr = $("#saving tbody tr");
-  output["7D"] = FormatRate(tr.eq(16).find("td").eq(2).text());
-  output["1M"] = FormatRate(tr.eq(17).find("td").eq(1).text());
-  output["3M"] = FormatRate(tr.eq(19).find("td").eq(1).text());
-  output["6M"] = FormatRate(tr.eq(20).find("td").eq(1).text());
-  output["12M"] = FormatRate(tr.eq(22).find("td").eq(1).text());
+  output["7D"] = FormatRate(tr.eq(15).find("td").eq(2).text());
+  output["1M"] = FormatRate(tr.eq(16).find("td").eq(1).text());
+  output["3M"] = FormatRate(tr.eq(18).find("td").eq(1).text());
+  output["6M"] = FormatRate(tr.eq(19).find("td").eq(1).text());
+  output["12M"] = FormatRate(tr.eq(21).find("td").eq(1).text());
 
   return {
     title: "星息定存 提取自如",
@@ -115,11 +115,11 @@ function getDetailWithCNY(html: string) {
   const output = GetInterestTemplate();
 
   const tr = $("#saving tbody tr");
-  output["7D"] = FormatRate(tr.eq(9).find("td").eq(2).text());
-  output["1M"] = FormatRate(tr.eq(10).find("td").eq(1).text());
-  output["3M"] = FormatRate(tr.eq(12).find("td").eq(1).text());
-  output["6M"] = FormatRate(tr.eq(13).find("td").eq(1).text());
-  output["12M"] = FormatRate(tr.eq(15).find("td").eq(1).text());
+  output["7D"] = FormatRate(tr.eq(8).find("td").eq(2).text());
+  output["1M"] = FormatRate(tr.eq(9).find("td").eq(1).text());
+  output["3M"] = FormatRate(tr.eq(11).find("td").eq(1).text());
+  output["6M"] = FormatRate(tr.eq(12).find("td").eq(1).text());
+  output["12M"] = FormatRate(tr.eq(14).find("td").eq(1).text());
 
   return {
     title: "星息定存 提取自如",
