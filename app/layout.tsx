8702a876ccd666263c181c8aa2f5775e7efecdb4
harrischen <harrischen@tencent.com>
2025-02-08 09:56:00 +0800

    feat: 添加google meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Bank Interest Rates',
   description: 'Compare bank interest rates in Hong Kong',
+  verification: {
+    google: 'rJf5dkvnZwcsNLqn2kR7_S5s22x_mm5XzFtLoCXIFaU',
+  },
 }
 
 export default function RootLayout({
